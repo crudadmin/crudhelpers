58fda12a2987721b0a9f387d002f6985577eccf3
Marek Gogol <info@marekgogol.sk>
2025-08-31 16:05:12 +0200

    ref

--- nuxt/plugins/01.locale.js
+++ nuxt/plugins/01.locale.js
@@ -2,7 +2,12 @@ import { defineNuxtPlugin } from '#app';
 import Translator from '../../utils/Translator';
 
 export default defineNuxtPlugin(async ({ vueApp, $pinia }) => {
-    let translator = new Translator(useLocaleStore($pinia).translations);
+    // We need pass pinia store to the translator
+    // Because this plugin may be loaded before NUXT pinia state is ready.
+    // And nuxt is using different store than classic vue apps.
+    const localeStore = useLocaleStore($pinia);
+
+    let translator = new Translator(localeStore.translations);
 
     await translator.install(vueApp);
 
