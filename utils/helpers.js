f3dbea888d79398afd7ed35663c13eb3468b1f7d
Marek Gogol <info@marekgogol.sk>
2025-08-31 18:37:21 +0200

    some helpers added

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,3 +9,77 @@ export const generateUuid = () => {
 
 export const useSleep = (delay) =>
     new Promise((resolve) => setTimeout(resolve, delay));
+
+/**
+ * Add loading progress on given element
+ */
+export const useLazyClick = async (e, callback) => {
+    let el = e.nodeName ? e : e.target;
+
+    //If clicked element is not button, try to find closest button
+    if (['BUTTON'].includes(el.nodeName) == false) {
+        el = el.closest('button, .icon-btn') || el;
+    }
+
+    if (el.loading) {
+        return;
+    }
+
+    el.loading = true;
+
+    el.setAttribute('disabled', true);
+    el.setAttribute('lazy-click-loading', true);
+
+    try {
+        if (callback) {
+            await callback(e);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    el.removeAttribute('disabled');
+    el.removeAttribute('lazy-click-loading');
+
+    el.loading = false;
+};
+
+export const useObjectToFormData = (obj, rootName, ignoreList) => {
+    var formData = new FormData();
+
+    function appendFormData(data, root) {
+        root = root || '';
+
+        if (data instanceof FormData) {
+            for (const [key, value] of data.entries()) {
+                formData.append(key, value);
+            }
+        } else if (data instanceof File) {
+            formData.append(root, data);
+        } else if (Array.isArray(data)) {
+            if (data.length == 0) {
+                formData.append(root, '');
+            } else {
+                for (var i = 0; i < data.length; i++) {
+                    appendFormData(data[i], root + '[' + i + ']');
+                }
+            }
+        } else if (typeof data === 'object' && data) {
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    if (root === '') {
+                        appendFormData(data[key], key);
+                    } else {
+                        appendFormData(data[key], root + '[' + key + ']');
+                    }
+                }
+            }
+        } else {
+            formData.append(root, _.isNil(data) ? '' : data);
+        }
+    }
+
+    appendFormData(obj, rootName);
+
+    return formData;
+};
