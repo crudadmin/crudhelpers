2a0cf926908db23444624f8b214350d8a560bdf8
Marek Gogol <info@marekgogol.sk>
2026-02-12 18:53:45 +0100

    added support for auth tokens

---
+++ nuxt/composables/editor.js
@@ -0,0 +1,73 @@
+export const installEditor = async () => {
+    const storageKey = 'ca_admin_token';
+
+    const token = getQueryParam('admin_token');
+    if (token) {
+        // Save to localStorage
+        try {
+            localStorage.setItem(storageKey, token);
+        } catch (e) {}
+
+        // Boot editor with new token
+        bootEditor(token);
+
+        // Redirect to root, stripping away the query params
+        window.location.href =
+            window.location.origin + window.location.pathname;
+    } else {
+        // Try to get token from localStorage
+        const storedToken = (() => {
+            try {
+                return localStorage.getItem(storageKey);
+            } catch (e) {
+                return null;
+            }
+        })();
+
+        if (storedToken) {
+            bootEditor(storedToken);
+        }
+    }
+};
+
+const bootEditor = async (token) => {
+    const data = await loadConfiguration();
+
+    window.CAEditorConfig.token = token;
+
+    data.styles.forEach((styleUrl) => injectLink(styleUrl));
+
+    data.scripts.forEach((scriptUrl) => injectScript(scriptUrl));
+};
+
+const loadConfiguration = async () => {
+    let response = await useAxios().get(
+            '/admin/api/frontend-editor/initialize'
+        ),
+        data = response.data;
+
+    window.CAEditorConfig = data.config;
+
+    return data;
+};
+
+const injectScript = (scriptUrl) => {
+    const scriptElem = document.createElement('script');
+    scriptElem.src = scriptUrl;
+    scriptElem.async = false;
+    document.head.appendChild(scriptElem);
+};
+
+const injectLink = (styleUrl) => {
+    const linkElem = document.createElement('link');
+    linkElem.rel = 'stylesheet';
+    linkElem.href = styleUrl;
+    document.head.appendChild(linkElem);
+};
+
+// Helper to get query params
+const getQueryParam = (param) => {
+    if (typeof window === 'undefined') return null;
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+};
